6ce145bcd858222714efa0bc0e3da6f5cd5be3e9
kpose <ganihujude@gmail.com>
2020-10-13 16:36:39 +0100

    background image and image weather function

--- App.tsx
+++ App.tsx
@@ -3,14 +3,15 @@ import {
   StyleSheet,
   View,
   Text,
-  TextInput,
   StatusBar,
-  KeyboardAvoidingView
+  ImageBackground,
+  KeyboardAvoidingView,
 } from 'react-native';
 
 
 import SearchInput from './src/components/SearchInput'
 
+import getImageForWeather from './src/utils/getImageForWeather'
 
 
 const App = () => {
@@ -18,12 +19,21 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <KeyboardAvoidingView style={styles.container} behavior='padding'> 
-        <Text style={styles.location}>Lagos, Nigeria</Text>
-        <Text style={styles.condition}>Weather Condition</Text>
-        <Text style={styles.temperature}>24°</Text>
-        
 
-        <SearchInput placeholder="Search any city" />
+      <ImageBackground
+        source={getImageForWeather('Hail')}
+        style={styles.imageContainer}
+        >
+
+         <View style = {styles.detailsContainer}>
+            <Text style={styles.location}>Lagos, Nigeria</Text>
+            <Text style={styles.condition}>Weather Condition</Text>
+            <Text style={styles.temperature}>24°</Text>
+            <SearchInput placeholder="Search any city" />
+         </View>
+
+        </ImageBackground>
+        
       </KeyboardAvoidingView>
     </>
   );
@@ -31,10 +41,8 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#ffffff',
+    backgroundColor: '#34495E',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   },
   location: {
     textAlign: 'center',
@@ -53,18 +61,21 @@ const styles = StyleSheet.create({
     fontFamily: 'Arial',
     fontSize: 20
   },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height:60,
-    width: 300,
-    marginTop: 10,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center',
-    borderRadius: 20
+  imageContainer: {
+    flex: 1
   },
-
+  /* image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode:'cover'
+  }, */
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20
+  }
 });
 
 export default App;
